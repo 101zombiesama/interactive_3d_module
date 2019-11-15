2e6f9f4c390306b786971c9abe4ce5ca99188f3d
Ajinkya Meshram <ajinkya.meshram@gmail.com>
2019-11-16 01:38:00 +0530

    production branch

--- main.js
+++ main.js
@@ -1,166 +1,190 @@
-let scene, camera, renderer;
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-
-function initViews(){
-    scene = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth/window.innerHeight,
-        0.1,
-        10000
-    );
-    camera.lookAt(new THREE.Vector3(0,0,0));
-    camera.position.set(0.1,0,-0.2);
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-};
-
-// Matreials
-var mat_gums = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load( "models/gum_COL.png" ),
-    metalness: 0,
-    normalMap: new THREE.TextureLoader().load( "models/gum_NRM.png" ),
-    normalScale: new THREE.Vector2( 1, 1 ),
-    roughness: 0.25,
-    side: THREE.DoubleSide
-});
-var mat_botteeth = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load( "models/botteeth_COL.png" ),
-    metalness: 0,
-    roughness: 0.3,
-    side: THREE.DoubleSide
-});
-var mat_topteeth = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load( "models/topteeth_COL.png" ),
-    metalness: 0,
-    roughness: 0.3,
-    side: THREE.DoubleSide
-});
-var mat_highlight = new THREE.MeshStandardMaterial({
-    color: new THREE.Color( 0xbffdff ),
-    metalness: 0,
-    roughness: 0.5,
-    emissive: new THREE.Color( 0xffffff ),
-    emissiveIntensity: 0.0,
-    side: THREE.DoubleSide
-});
-
-// Loading External Mesh
-var teethModel;
-
-function initModel(){
-    var loader = new THREE.OBJLoader();
-    loader.load(
-        "./models/teeth.obj",
-        function ( object ) {
-            teethModel = object;
-
-            object.children[0].material = mat_gums; //gums lower
-            object.children[1].material = mat_botteeth;  //teeth lower
-            object.children[2].material = mat_gums;  //gums upper
-            object.children[3].material = mat_topteeth;  //teeth upper
-            scene.add( object );
-
-        },
-        function ( xhr ) {
-
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-        },
-        function ( error ) {
-            console.log(error.target);
-            console.log( 'An error happened' );
-
-        }
-    );
-
-}
-
-
-function initLights(){
-    // Lights
-    var leftLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 50%, 75%)'), 0.5);
-    leftLight.position.set(-100, 0, 20);
-
-    var rightLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 50%, 75%)'), 0.5);
-    rightLight.position.set(100, 0, 100);
-
-    var frontLight_r = new THREE.DirectionalLight(0xffffff, 0.5);
-    frontLight_r.position.set(100, 0, -75).normalize();
-
-    var frontLight_l = new THREE.DirectionalLight(0xffffff, 0.5);
-    frontLight_l.position.set(-100, 0, -75).normalize();
-
-    var ambLight = new THREE.AmbientLight( 0x404040 );
-
-    scene.add(leftLight);
-    scene.add(rightLight);
-    scene.add(frontLight_r);
-    scene.add(frontLight_l);
-    scene.add(ambLight);
-
-    var axesHelper = new THREE.AxesHelper( 5 );
-    // scene.add( axesHelper );
-}
-
-// initiating
-initViews();
-initModel();
-initLights();
-
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-
 // functions event listeners
-function resetCameraView(){
-    controls.target.copy(teethModel.position);
+
+var isMouseDown = false;
+var mousePosBeforeClick = {};
+var mousePosAfterClick = {};
+window.addEventListener('mousedown', event => {
+    isMouseDown = true;
+    mousePosBeforeClick.x = event.clientX;
+    mousePosBeforeClick.y = event.clientY;
+})
+window.addEventListener('mouseup', event => {
+    isMouseDown = false;
+    mousePosAfterClick.x = event.clientX;
+    mousePosAfterClick.y = event.clientY;
+})
+
+// set slider value to zero on window load
+var slider = document.getElementById('animBlend');
+slider.value = 0;
+
+
+function resetCameraView() {
+    controls.target.copy(upper_gum_model.position);
 };
 
+// reset orbit pivot
 document.addEventListener('keydown', event => {
-    if(event.key === 'f'){
+    if (event.key === 'f') {
         resetCameraView();
     }
 });
+
+// handle viewer resize when window resized
 window.addEventListener('resize', event => {
-    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-})
+});
+
 // handling mouth open animation with slider
 var slider = document.getElementById('animBlend');
 slider.addEventListener('input', event => {
-    teethModel.children[0].rotation.x = -slider.value/2;
-    teethModel.children[0].position.y = -slider.value/30;
-    teethModel.children[1].rotation.x = -slider.value/2;
-    teethModel.children[1].position.y = -slider.value/30;
-})
-// onHover material change
-
-window.addEventListener( 'mousemove', event => {
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-    raycaster.setFromCamera( mouse, camera );
-    var intersects = raycaster.intersectObjects( teethModel.children );
-
-    teethModel.children[0].material = mat_gums; //gums lower
-    teethModel.children[1].material = mat_botteeth;  //teeth lower
-    teethModel.children[2].material = mat_gums;  //gums upper
-    teethModel.children[3].material = mat_topteeth;  //teeth upper
-
-    if(intersects.length > 0){
-        intersects[0].object.material = mat_highlight;
-    }
-
+    lower_teeth_model.rotation.x = -slider.value / 2;
+    lower_teeth_model.position.y = -slider.value / 30;
+    lower_gum_model.rotation.x = -slider.value / 2;
+    lower_gum_model.position.y = -slider.value / 30;
 });
 
-function animate() {
-    requestAnimationFrame( animate );
+window.addEventListener('mousemove', event => {
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    }
+);
 
-    controls.update();
-    renderer.render( scene, camera );
+
+var selectedTooth;
+
+function updateSelectedTooth(tooth){
+    for (let child of lower_teeth_model.children) {
+        child.material = mat_master;
+    }
+    for (let child of upper_teeth_model.children) {
+        child.material = mat_master;
+    }
+    tooth.material = mat_selected;
+    selectedTooth = tooth;
 }
-animate();
+
+function clearSelection(){
+    for (let child of lower_teeth_model.children) {
+        child.material = child.requiredMaterial
+    }
+    for (let child of upper_teeth_model.children) {
+        child.material = child.requiredMaterial
+    }
+    selectedTooth = null;
+}
+
+function clearHighlight(){
+    for (let child of lower_teeth_model.children) {
+        child.material = child.requiredMaterial
+    }
+    for (let child of upper_teeth_model.children) {
+        child.material = child.requiredMaterial
+    }
+}
+
+
+
+window.onload = () => {
+
+    // click in empty area to clear selection
+    window.addEventListener('click', event => {
+        raycaster.setFromCamera(mouse, camera);
+        var meshes = [];
+        scene.traverse( (child) => {
+            if (child.isMesh) {
+                meshes.push(child)
+            }
+        } );
+        var intersects = raycaster.intersectObjects(meshes);
+        if (intersects.length <= 0 && JSON.stringify(mousePosBeforeClick) == JSON.stringify(mousePosAfterClick)) {
+            clearSelection();
+        }
+
+    });
+
+    // hover on no-teeth area to clear highlight
+    window.addEventListener('mousemove', event => {
+        raycaster.setFromCamera(mouse, camera);
+        var intersects = raycaster.intersectObjects([...lower_teeth_model.children, ...upper_teeth_model.children]);
+        if (intersects.length <= 0) {
+            // console.log("clearing highlight");
+            clearHighlight();
+            if(selectedTooth){
+                selectedTooth.material = mat_selected;
+            }
+        }
+
+    });
+
+    // adding event listeners to all teeth
+    for (let tooth of lower_teeth_model.children) {
+        domEvents.addEventListener(tooth, 'click', event => {
+            // if statement for excluding drag as a click
+            if (JSON.stringify(mousePosAfterClick) == JSON.stringify(mousePosBeforeClick)){
+                updateSelectedTooth(tooth);
+            }
+            
+        })
+
+        domEvents.addEventListener(tooth, 'mouseover', event => {
+            if (selectedTooth) {
+                if (!isMouseDown &&  tooth.uuid != selectedTooth.uuid) {
+                    clearHighlight();
+                    tooth.material = mat_highlight;
+                    selectedTooth.material = mat_selected;
+                }
+            } 
+            else if(!isMouseDown) {
+                clearHighlight();
+                tooth.material = mat_highlight;
+            }
+        });
+
+        // domEvents.addEventListener(tooth, 'mouseout', event => {
+        //     if (selectedTooth) {
+        //         // reverting back to required material when moouseout and this tooth is not selected tooth
+        //         if (!isMouseDown &&  tooth.uuid != selectedTooth.uuid) {
+        //             tooth.material = tooth.requiredMaterial;
+        //         }
+        //     } else {
+        //         tooth.material = tooth.requiredMaterial;
+        //     }
+        // });
+    }
+    for (let tooth of upper_teeth_model.children) {
+
+        domEvents.addEventListener(tooth, 'click', event => {
+            // if statement for excluding drag as a click
+            if (JSON.stringify(mousePosAfterClick) == JSON.stringify(mousePosBeforeClick)){
+                updateSelectedTooth(tooth);
+            }
+            
+        })
+
+        domEvents.addEventListener(tooth, 'mouseover', event => {
+            if (selectedTooth) {
+                if (!isMouseDown &&  tooth.uuid != selectedTooth.uuid) {
+                    clearHighlight();
+                    tooth.material = mat_highlight;
+                    selectedTooth.material = mat_selected;
+                }
+            } else if(!isMouseDown) {
+                clearHighlight();
+                tooth.material = mat_highlight;
+            }
+        });
+
+        // domEvents.addEventListener(tooth, 'mouseout', event => {
+        //     if (selectedTooth) {
+        //         if (!isMouseDown &&  tooth.uuid != selectedTooth.uuid) {
+        //             tooth.material = tooth.requiredMaterial;
+        //         }
+        //     } else {
+        //         tooth.material = tooth.requiredMaterial;
+        //     }
+        // });
+    }
+}
---
+++ viewer.js
@@ -0,0 +1,193 @@
+let scene, camera, renderer;
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+function initViews(){
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth/window.innerHeight,
+        0.01,
+        10000
+    );
+    camera.lookAt(new THREE.Vector3(0,0,0));
+    camera.position.set(0.1,0,-0.2);
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+};
+
+// Matreials
+var mat_master = new THREE.MeshStandardMaterial({
+    // color: new THREE.Color('hsl(240, 50%, 75%)'),
+    map: new THREE.TextureLoader().load( "models/teeth/Albedo(AO).png" ),
+    metalness: 0,
+    normalMap: new THREE.TextureLoader().load( "models/teeth/Normal.png" ),
+    normalScale: new THREE.Vector2( 1, 1 ),
+    roughnessMap: new THREE.TextureLoader().load( "models/teeth/Roughness.png" ),
+    side: THREE.DoubleSide
+});
+var mat_gums = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load( "models/gum_COL.png" ),
+    metalness: 0,
+    normalMap: new THREE.TextureLoader().load( "models/gum_NRM.png" ),
+    normalScale: new THREE.Vector2( 1, 1 ),
+    roughness: 0.25,
+    side: THREE.DoubleSide
+});
+var mat_botteeth = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load( "models/botteeth_COL.png" ),
+    metalness: 0,
+    roughness: 0.3,
+    side: THREE.DoubleSide
+});
+var mat_topteeth = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load( "models/topteeth_COL.png" ),
+    metalness: 0,
+    roughness: 0.3,
+    side: THREE.DoubleSide
+});
+var mat_highlight = new THREE.MeshStandardMaterial({
+    color: new THREE.Color( 0xbffdff ),
+    metalness: 0,
+    roughness: 0.5,
+    emissive: new THREE.Color( 0xffffff ),
+    emissiveIntensity: 0.0,
+    side: THREE.DoubleSide
+});
+var mat_selected = new THREE.MeshStandardMaterial({
+    color: new THREE.Color( 0xbbff99 ),
+    metalness: 0,
+    roughness: 0.5,
+    emissive: new THREE.Color( 0xffffff ),
+    emissiveIntensity: 0.0,
+    side: THREE.DoubleSide
+});
+
+// Loading External Mesh
+var upper_teeth_model;
+var lower_teeth_model;
+var upper_gum_model;
+var lower_gum_model;
+
+function initModel(){
+    var loader = new THREE.OBJLoader();
+    loader.load(
+        "./models/teeth/bottom_teeth.obj",
+        function ( object ) {
+            lower_teeth_model = object;
+            for (i=0; i<object.children.length; i++ ) {
+                object.children[i].requiredMaterial = mat_master;
+                object.children[i].material = object.children[i].requiredMaterial;
+            }
+            scene.add( object );
+
+        },
+        function ( xhr ) {},
+        function ( error ) {
+            console.log(error.target);
+            console.log( 'An error happened' );
+
+        }
+    );
+
+    loader.load(
+        "./models/teeth/upper_teeth.obj",
+        function ( object ) {
+            upper_teeth_model = object;
+            for (i=0; i<object.children.length; i++ ) {
+                object.children[i].requiredMaterial = mat_master;
+                object.children[i].material = object.children[i].requiredMaterial;
+            }
+            scene.add( object );
+
+        },
+        function ( xhr ) {},
+        function ( error ) {
+            console.log(error.target);
+            console.log( 'An error happened' );
+
+        }
+    );
+
+    loader.load(
+        "./models/teeth/lower_gum.obj",
+        function ( object ) {
+            lower_gum_model = object;
+            for (i=0; i<object.children.length; i++ ) {
+                object.children[i].material = mat_master;
+            }
+            scene.add( object );
+
+        },
+        function ( xhr ) {},
+        function ( error ) {
+            console.log(error.target);
+            console.log( 'An error happened' );
+
+        }
+    );
+
+    loader.load(
+        "./models/teeth/upper_gum.obj",
+        function ( object ) {
+            upper_gum_model = object;
+            for (i=0; i<object.children.length; i++ ) {
+                object.children[i].material = mat_master;
+            }
+            scene.add( object );
+
+        },
+        function ( xhr ) {},
+        function ( error ) {
+            console.log(error.target);
+            console.log( 'An error happened' );
+
+        }
+    );
+
+}
+
+
+function initLights(){
+    // Lights
+    var leftLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 50%, 75%)'), 0.5);
+    leftLight.position.set(-100, 0, 20);
+
+    var rightLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 50%, 75%)'), 0.5);
+    rightLight.position.set(100, 0, 100);
+
+    var frontLight_r = new THREE.DirectionalLight(0xffffff, 0.5);
+    frontLight_r.position.set(100, 0, -75).normalize();
+
+    var frontLight_l = new THREE.DirectionalLight(0xffffff, 0.5);
+    frontLight_l.position.set(-100, 0, -75).normalize();
+
+    var ambLight = new THREE.AmbientLight( 0x404040 );
+
+    scene.add(leftLight);
+    scene.add(rightLight);
+    scene.add(frontLight_r);
+    scene.add(frontLight_l);
+    scene.add(ambLight);
+
+    var axesHelper = new THREE.AxesHelper( 5 );
+    // scene.add( axesHelper );
+}
+
+// initiating
+initViews();
+initModel();
+initLights();
+
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+const domEvents = new THREEx.DomEvents(camera, renderer.domElement);
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    controls.update();
+    renderer.render( scene, camera );
+}
+animate();
